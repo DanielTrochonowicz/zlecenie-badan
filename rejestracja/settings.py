788ebc1e7a64817b379f5a40583e99731644e6d6
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2020-03-15 03:41:00 +0100

    update

--- rejestracja/settings.py
+++ rejestracja/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['zlecenie-badan.herokuapp.com/', 'localhost:8000']
+ALLOWED_HOSTS = ['https://zlecenie-badan.herokuapp.com/ZleceniaBadan/zlecenia/', 'localhost:8000']
 
 
 # Application definition
