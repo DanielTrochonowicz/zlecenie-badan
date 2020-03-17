68f2f1fee99b92ecde0eb968d3e24e090c03e133
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2020-03-17 13:53:16 +0100

    update

--- ZleceniaBadan/tests.py
+++ ZleceniaBadan/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from selenium import webdriver
 # Create your tests here.
+
+
+driver = webdriver.Chrome(executable_path=r"C:\TestFiles\chromedriver.exe")
+driver.get('https://zlecenie-badan.herokuapp.com/ZleceniaBadan/zlecenia/')
+title = driver.title
+print(title)
+assert '' == title
+driver.quit()
